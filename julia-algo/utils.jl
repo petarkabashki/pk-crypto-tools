8284158338ec9fa56b291270c37c136da6b5ea31
Petar Kabashki <peter.kabashky@gmail.com>
2021-06-04 09:02:51 +0100

    wip

--- julia-algo/utils.jl
+++ julia-algo/utils.jl
@@ -6,19 +6,27 @@ function window_slider(len)
     
     log2_slider = slider(1:log2len, label = "2^n els/wnd", default = log2len/2)
     window_size = Interact.@map (2 ^ &log2_slider)
-    window_no = Observable{Int64}(1)
+    window_start = Observable{Int64}(1)
     
     function make_startslider(wnd_size) 
-        window_start_slider = slider(1:Int(trunc(len/wnd_size)); label="window#", default=1)
+        window_start_slider = slider(1:Int(trunc(len/wnd_size*5)); label="window#", default=1)
         
-        map!(x -> x , window_no, window_start_slider)
+        map!(x -> Int64(trunc(x/5*window_size[])) , window_start, window_start_slider)
         dom"div"(window_start_slider)
     end
     
     map!(make_startslider, start_slider_box, window_size)
 #     output = Interact.@map (&window_no * &window_size , (1 + &window_no) * &window_size - 1)
-    output = Interact.@map (&window_no , &window_size)
+    output = Interact.@map (&window_start , &window_size)
     
-    wdg = Widget(["log2_slider" => log2_slider, "start_slider_box" => start_slider_box], output = output)
-    @layout! wdg vbox(:log2_slider, :start_slider_box) ## custom layout: by default things are stacked vertically
+    wdg = Widget(["log2_slider" => log2_slider, "start_slider_box" => start_slider_box, "window_start" => window_start,
+            "window_size" => window_size], output = output)
+    @layout! wdg vbox(:start_slider_box, :log2_slider) ## custom layout: by default things are stacked vertically
+end
+
+function calc_wavelet(ar, m, wt)
+#     wt = wavelet(wvlet)
+    xt = dwt(ar, wt)
+    threshold!(xt, BiggestTH(), m)
+    fxt = idwt(xt, wt)
 end
