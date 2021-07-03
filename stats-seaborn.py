0a6a38d91db0dd73447ab190f055ad42d229db1e
Petar Kabashki <peter.kabashky@gmail.com>
2021-07-03 06:03:02 +0100

    wip

--- stats-seaborn.py
+++ stats-seaborn.py
@@ -35,8 +35,9 @@ df.set_index(pd.DatetimeIndex(df["time"]), inplace=True, drop=True)
 # start_time = pd.Timestamp('now').floor('D') + pd.Timedelta(-7, unit='D')
 
 # start_time = pd.Timestamp('now') + pd.Timedelta(-2, unit='W')
-start_time = datetime(2021,6,1)
-end_time = datetime(2021,6,8)
+
+start_time = datetime(2021,5,1)
+end_time = datetime(2021,7,1)
 
 ddf = df.loc[(df.index >= start_time) & (df.index <= end_time)].copy()
 # ddf = df.loc[df['time'] >= start_time]
@@ -48,7 +49,12 @@ ddf['fema'] = talib.EMA(ddf.close, 50)
 ddf['sema'] = talib.EMA(ddf.close, 120)
 
 tper = int(60 * 3)
-ddf['ret'] = ddf.close - ddf.close.shift()
+
+# ddf['ret'] = ddf.close - ddf.close.shift(fill_value=0)
+# ddf['pct_change'] = ddf.close.pct_change()
+# ddf['sret'] = ddf.close / ddf.close.shift(fill_value=ddf.close[0])
+# ddf['lret'] =   np.diff(np.log(ddf.close))
+ddf['lret'] = np.log(ddf.close.astype('float32')/ddf.close.astype('float32').shift(1))
 ddf['fufema'] = ddf.fema.shift(-tper)
 ddf['fumin'] = ddf.close.rolling(tper).min().shift(-tper)
 ddf['fumax'] = ddf.close.rolling(tper).max().shift(-tper)
@@ -62,6 +68,8 @@ ddf['macdperc'] = ddf.macd / ddf.sema * 100
 
 macd_gt0_mask = (ddf['macd'] > 0)
 macd_lt0_mask = ~(macd_gt0_mask)
+ddf['macd_gt0_mask'] = macd_gt0_mask
+ddf['macd_lt0_mask'] = macd_lt0_mask
 
 # ddf['macd_gt0'] = macd_gt0_mask
 cross_up_mask = (
@@ -72,41 +80,80 @@ cross_dn_mask = (
         (macd_lt0_mask & macd_gt0_mask.shift(1).astype(bool))
     )
 
-g_cross_up = cross_up_mask.shift(fill_value=0).cumsum()
-ddf['cross_up_len'] = ddf.groupby(g_cross_up).cumcount()
-ddf['cross_up_cumret'] = ddf.ret.groupby(g_cross_up).cumsum()
+cross_mask = cross_up_mask | cross_dn_mask
+g_cross = cross_mask.shift(fill_value=0).cumsum()
+ddf['g_cross'] = g_cross
+ddf['cross_since'] = ddf.groupby(g_cross).cumcount()
+ddf['cross_cumlret'] = ddf.lret.groupby(g_cross).cumsum()
 
-bdf = ddf[(ddf['macd']>=0) & (ddf['cross_up_len'] < 200)]
+ddf['trend_len'] = ddf.groupby(['macd_gt0_mask','g_cross']).cross_since.transform('max')
+ddf['max_ret'] = ddf.groupby(['macd_gt0_mask','g_cross']).cross_cumlret.transform('max')
+ddf['min_ret'] = ddf.groupby(['macd_gt0_mask','g_cross']).cross_cumlret.transform('min')
 
-wdf = ddf.iloc[1:24*60*2, :]
-wdf_up_crosses = wdf[wdf['cross_up_len'] == 0]
+# g_cross_up = cross_up_mask.shift(fill_value=0).cumsum()
+# ddf['g_cross_up'] = g_cross_up
+# ddf['cross_up_len'] = ddf.groupby(g_cross_up).cumcount()
+# # ddf['cross_up_cumret'] = ddf.ret.groupby(g_cross_up).cumsum()
+# ddf['cross_up_cumlret'] = ddf.groupby(g_cross_up).lret.cumsum()
+# ddf['cross_up_cum_percret'] = np.exp(ddf['cross_up_cumlret']) - 1
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, gridspec_kw={'height_ratios':[2,1,1]})
+# bdf = ddf[(ddf['macd']>=0) & (ddf['cross_up_len'] < 200)]
+
+wdf = ddf.iloc[1:24*60*2, :].copy()
+wdf_crosses = wdf[wdf['cross_since'] == 0]
+# set downtrend values to 0
+# wdf.loc[macd_lt0_mask, ['cross_cumlret', 'max_ret', 'min_ret', 'cross_since', 'trend_len']] = 0
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True, gridspec_kw={'height_ratios':[2,1,1,1]})
 ax1.plot(wdf.close, lw=0.5)
 #ax1.plot(wdf.ema26, lw=0.5)
 ax1.plot(wdf.fema, lw=0.5)
 ax1.plot(wdf.sema, lw=0.5)
-# ax1.plot(wdf.fumin, lw=0.5)
-# ax1.plot(wdf.fumax, lw=0.5)
-# ax1.plot(wdf.fufema, lw=1.5)
-ax1.vlines(wdf_up_crosses.index, wdf.close.min(), wdf.close.max(),lw=0.5)
+ax1.fill_between(wdf.index, y1=wdf.fema*0.995, y2=wdf.fema*1.005, alpha=0.2 )
+ax1.vlines(wdf_crosses.index, wdf.close.min(), wdf.close.max(),lw=0.5)
 
-ax2.plot(wdf.trend_up_cumret, lw=0.5)
-# ax2.plot(wdf.fuminperc, lw=0.5)
-# ax2.plot(wdf.fumaxperc, lw=0.5)
-#ax2.plot(wdf.macdperc, lw=0.5)
+ax2.plot(wdf.cross_cumlret, lw=0.5)
+ax2.plot(wdf.max_ret, lw=0.5)
+ax2.plot(wdf.min_ret, lw=0.5)
 ax2.axhline(lw=0.5)
-ax2.vlines(wdf_up_crosses.index, wdf.cross_up_cumret.min(), wdf.cross_up_cumret.max(),lw=0.5)
+ax2.vlines(wdf_crosses.index, wdf.cross_cumlret.min(), wdf.cross_cumlret.max(),lw=0.5)
+ax2.fill_between(wdf.index, wdf.cross_cumlret.min(), wdf.cross_cumlret.max(),alpha=0.9, 
+                 color='grey', where=wdf.macd_lt0_mask)
 
-ax3.plot(wdf.trend_up_len, lw=0.5)
-# ax3.plot(wdf.macd, lw=0.5)
-#ax3.plot(wdf.fumaxperc, lw=0.5)
+ax3.plot(wdf.cross_since, lw=0.5)
+ax3.plot(wdf.trend_len, lw=0.5)
 ax3.axhline(lw=0.5)
-ax3.vlines(wdf_up_crosses.index, wdf.cross_up_len.min(), wdf.cross_up_len.max(),lw=0.5)
+ax3.vlines(wdf_crosses.index, wdf.cross_since.min(), wdf.cross_since.max(),lw=0.5)
+ax3.fill_between(wdf.index, wdf.cross_since.min(), wdf.cross_since.max(),alpha=0.9, 
+                 color='grey', where=wdf.macd_lt0_mask)
 
-#fig.show()
+ax4.plot(wdf.g_cross)
+ax4.vlines(wdf_crosses.index, wdf.g_cross.min(), wdf.g_cross.max(),lw=0.5)
+ax4.fill_between(wdf.index, wdf.g_cross.min(), wdf.g_cross.max(),alpha=0.9, 
+                 color='grey', where=wdf.macd_lt0_mask)
 
-sns.displot(bdf, x="fufemaperc", y="trend_len", cbar=True)
+
+# 
+
+
+# distribution of period of uptrend > 0
+zdf = ddf[macd_gt0_mask].groupby(g_cross).cross_since.max()
+sns.displot(zdf, binwidth=5)
+
+
+# distribution of max cumulative log returns when macd > 0
+# zdf = ddf[macd_gt0_mask].groupby(g_cross).cross_cumlret.max()
+sns.displot(ddf[macd_gt0_mask].max_ret)
+
+# distribution of min cumulative log returns when macd > 0
+zdf = ddf[macd_gt0_mask].groupby(g_cross).cross_cumlret.min()
+sns.displot(zdf)
+
+
+zdf = ddf[macd_gt0_mask].groupby(g_cross).first()
+
+sns.displot(zdf, x="trend_len", y="max_ret", hue='min_ret', cbar=False)
+
 sns.displot(bdf, x="fumaxperc", y="trend_len", cbar=True)
 
 sns.jointplot(data=bdf, x="fufemaperc", y="trend_len", hue="fuminperc", s=1)
