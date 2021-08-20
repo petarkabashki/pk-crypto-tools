f6570b1b71470098fd412d3bb52a61557af1e1f1
Petar Kabashki <peter.kabashky@gmail.com>
2021-08-21 00:59:58 +0100

    binance, kucoin

--- pk-bot/load-creds.sh
+++ pk-bot/load-creds.sh
@@ -5,3 +5,11 @@ export kucoin_apiTest=$(vault kv get -field=test secret/pkbot/kucoin/api)
 export kucoin_apiKey=$(vault kv get -field=apiKey secret/pkbot/kucoin/api)
 export kucoin_apiSecret=$(vault kv get -field=secret secret/pkbot/kucoin/api)
 export kucoin_apiPassphrase=$(vault kv get -field=passphrase secret/pkbot/kucoin/api)
+
+export binance_apiTest=$(vault kv get -field=test secret/pkbot/binance/api)
+export binance_apiKey=$(vault kv get -field=apiKey secret/pkbot/binance/api)
+export binance_apiSecret=$(vault kv get -field=secret secret/pkbot/binance/api)
+
+export ftx_apiTest=$(vault kv get -field=test secret/pkbot/ftx/api)
+export ftx_apiKey=$(vault kv get -field=apiKey secret/pkbot/ftx/api)
+export ftx_apiSecret=$(vault kv get -field=secret secret/pkbot/ftx/api)
