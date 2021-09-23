7abbeba50b543db9d0666dc74b22ca9f1331987c
Petar Kabashki <peter.kabashky@gmail.com>
2021-09-23 15:52:47 +0100

    wip

--- pk-bot/bot.py
+++ pk-bot/bot.py
@@ -1,6 +1,7 @@
-
+#%%
+import os
 import ccxt
-import config
+# import config
 import schedule
 import pandas as pd
 pd.set_option('display.max_rows', None)
@@ -8,43 +9,89 @@ from datetime import datetime
 import time
 import pytz
 import json
-
+import numpy as np
 # import warnings
 # warnings.filterwarnings('ignore')
 import sys
-sys.path.insert(1, './strategy')
+# sys.path.insert(1, './strategy')
 from importlib import reload  
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from pprint import pprint
+#%%
+exchanges = {}
+config = False
+with open ('config.json') as js: 
+        config = json.load(js)
 
-exchange = ccxt.kucoin({
-    "apiKey": config.KUCOIN_API_KEY,
-    "secret": config.KUCOIN_SECRET_KEY,
-    "password": config.KUCOIN_API_PASSPHRASE
-})
+for exkey, exconf in config['exchanges'].items():
+        if exconf['enabled'] and not exkey in exchanges:
+                excred = exconf['cred']
+                for crk, crv in excred.items():
+                        if crv[0] == "$" : 
+                                excred[crk] = os.getenv(crv[1:])
 
+                exchanges[exkey] = getattr(ccxt, exkey) (excred)
+
+#%%
+client = MongoClient()
+db=client.pkbot
+
+tickers = {}
 
 def run_bot():
-    print(f'{datetime.now()} Checking for new orders...')
-    odf = pd.read_csv ('order-data/orders.csv', index_col='id', dtype={'id': 'string', 'orderId': 'string'})
-    ondf = odf[odf.orderId.isnull()]
-    pairs = ondf.pair.values
-    tickers = exchange.fetchTickers (pairs)
+  global exchanges, tickers
+  
+  print("Scanning for new orders ...")
+  for csyms in db.xqueue.aggregate([    
+      {'$match':{'status': "new"}},
+      {'$unwind':"$q"},
+      {'$group':{'_id': {'exchange':"$q.exchange", "symbol": "$q.symbol"}}},
+      {'$group':{'_id': '$_id.exchange',"symbols": {'$addToSet': "$_id.symbol"}}},
+      # {'$match':{'_id': qo['exchange']}}
+  ]):
+    exchange_name = csyms['_id']
+    symbols = csyms['symbols']
+    tickers[exchange_name] = exchanges[exchange_name].fetchTickers(symbols)
+    # print(exchange_name, tickers[exchange_name])
+  for qx in db.xqueue.find({'status' :'new'}):
+    print(f"Processing q/{qx['_id']}")
+    for idxqo, qo in enumerate(qx['q']):
+      nqx = db.xqueue.find_one({'_id': qx.get('_id')})
+      print(f'   qid/{qo["qid"]} status: {qo["status"]}')
+      exchange_name = qo['exchange']
+      symbol = qo['symbol']
+      if qo['status'] != 'new':
+        continue
 
-    for oind, orow in ondf.iterrows():
-        # print("index", index)
-        module = __import__(orow.strategy)
-        order_class = getattr(module, orow.strategy)
-        order_params = json.loads(orow.order_params)
-        order_instance = order_class(exchange, orow.pair, tickers[orow.pair], order_params)
-        order = order_instance.execute()
-        if order:
-            odf.loc[oind, "orderId"] = order['id']
-            odf.to_csv('order-data/orders.csv', index=True)
-    # odf
+      print(f'        Checking {exchange_name}: qid/{qo["qid"]} {qo["trigger"]}')
+      
+      exchange = exchanges[exchange_name]
+      ticker = tickers[exchange_name][symbol]
+
+      if not eval(qo['trigger'], {'ticker': ticker, 'qx' : nqx}):
+        continue
+
+      print(f'        Triggered {exchange_name}:  {qo["method"]}')
+      # res = getattr(exchange, qo['method'])(*qo['margs'])
+      res = eval(qo['method'])
+      print('RES:', res)
+      updres = db.xqueue.update_one({'_id' : qx.get('_id') }, {
+        '$set': { 
+          # f"q.{idxqo}.orderId": res['id'],
+          f"q.{idxqo}.status": 'done',
+          f"q.{idxqo}.res": res,
+          }
+      })
+
+    if db.xqueue.find_one({'_id': qx.get('_id'), "q.status" : { "$ne": "done" }}) == None:
+      db.xqueue.update_one({'_id' : qx.get('_id') }, {
+          '$set': { 'status': 'done' }
+        })
 
 
-schedule.every(10).seconds.do(run_bot)
-
-
+schedule.every(5).seconds.do(run_bot)
 while True:
     schedule.run_pending()
     time.sleep(1)
+# run_bot()
--- pk-bot/load-creds.sh
+++ pk-bot/load-creds.sh
@@ -1,15 +1,15 @@
 # stty -echoctl
 vault login $(cat ~/vault/pkbot.token.txt) 1> /dev/null
 # stty echoctl
-export kucoin_apiTest=$(vault kv get -field=test secret/pkbot/kucoin/api)
-export kucoin_apiKey=$(vault kv get -field=apiKey secret/pkbot/kucoin/api)
-export kucoin_apiSecret=$(vault kv get -field=secret secret/pkbot/kucoin/api)
-export kucoin_apiPassphrase=$(vault kv get -field=passphrase secret/pkbot/kucoin/api)
+# export kucoin_apiTest=$(vault kv get -field=test secret/pkbot/kucoin/api)
+# export kucoin_apiKey=$(vault kv get -field=apiKey secret/pkbot/kucoin/api)
+# export kucoin_apiSecret=$(vault kv get -field=secret secret/pkbot/kucoin/api)
+# export kucoin_apiPassphrase=$(vault kv get -field=passphrase secret/pkbot/kucoin/api)
 
 export binance_apiTest=$(vault kv get -field=test secret/pkbot/binance/api)
 export binance_apiKey=$(vault kv get -field=apiKey secret/pkbot/binance/api)
 export binance_apiSecret=$(vault kv get -field=secret secret/pkbot/binance/api)
 
-export ftx_apiTest=$(vault kv get -field=test secret/pkbot/ftx/api)
-export ftx_apiKey=$(vault kv get -field=apiKey secret/pkbot/ftx/api)
-export ftx_apiSecret=$(vault kv get -field=secret secret/pkbot/ftx/api)
+# export ftx_apiTest=$(vault kv get -field=test secret/pkbot/ftx/api)
+# export ftx_apiKey=$(vault kv get -field=apiKey secret/pkbot/ftx/api)
+# export ftx_apiSecret=$(vault kv get -field=secret secret/pkbot/ftx/api)
